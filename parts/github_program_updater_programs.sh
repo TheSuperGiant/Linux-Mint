b9bba00f1a8dbda3bbbd03979d0db35a31ff3947
TheSuperGiant <42948694+TheSuperGiant@users.noreply.github.com>
2025-11-17 10:56:21 +0000

    Add files via upload

--- parts/github_program_updater_programs.sh
+++ parts/github_program_updater_programs.sh
@@ -4,15 +4,18 @@ declare -a github_program_updater_programs=(
 	"thorium:	thorium; Alex313031; -s;thorium-browser.*; -R;1"
 	"winboat:	winboat; tibixdev"
 )
-for github_program in "${github_program_updater_programs[@]}"; do
-	program_name="${github_program%%:*}"
-	prameters="${github_program##*:}"
-	if [ "$(eval echo \$App_Install__$program_name)" == "1" ]; then
-		IFS=';' read -ra prameters_parts <<< "${prameters}"
-		for i in "${!prameters_parts[@]}"; do
-			prameters_parts[$i]="$(sed 's/^[[:space:]]*//' <<<"${prameters_parts[$i]}")"
-		done
-		echo "$program_name"
-		github_program_updater -r "${prameters_parts[0]}" -u "${prameters_parts[1]}" "${prameters_parts[@]:2}"
-	fi
-done
+github_program_updater_programs(){
+	for github_program in "${github_program_updater_programs[@]}"; do
+		program_name="${github_program%%:*}"
+		prameters="${github_program##*:}"
+		if [ "$(eval echo \$App_Install__$program_name)" == "1" ]; then
+			IFS=';' read -ra prameters_parts <<< "${prameters}"
+			for i in "${!prameters_parts[@]}"; do
+				prameters_parts[$i]="$(sed 's/^[[:space:]]*//' <<<"${prameters_parts[$i]}")"
+			done
+			echo "$program_name"
+			#github_program_updater -r "${prameters_parts[0]}" -u "${prameters_parts[1]}" "${prameters_parts[@]:2}"
+			github_program_updater -r "${prameters_parts[0]}" -u "${prameters_parts[1]}" "${prameters_parts[@]:2}" "$1"
+		fi
+	done
+}
